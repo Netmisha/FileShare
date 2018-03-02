0124a7f84f0a5651e4b7d07bda2577271bac2739
OlehKostiv <oleh.kostiv@gmail.com>
2018-03-02 17:37:09 +0200

    made a bunch'o empty classes as model component substitutes

--- FileShare/Model.h
+++ FileShare/Model.h
@@ -1,4 +1,26 @@
 #pragma once
 
-class Model
-{};
+#include "MessengerComponent.h"
+#include "SharedFolderNavigatorComponent.h"
+#include "UserDataFileComponent.h"
+#include "PresenceComponent.h"
+
+namespace FileShare {
+
+    class MessengerComponent;
+    class SharedFolderNavigatorComponent;
+    class UserDataFileComponent;
+    class PresenceComponent;
+
+    class Model
+    {
+    public:
+
+    protected:
+        MessengerComponent chatMessenger;
+        SharedFolderNavigatorComponent sfNavigator;
+        UserDataFileComponent dataFileNavigator;
+        PresenceComponent yetToGetAGoodName;
+        friend class ConsoleController;
+    };
+}
---
+++ FileShare/PresenceComponent.h
@@ -0,0 +1,6 @@
+#pragma once
+
+namespace FileShare {
+    class PresenceComponent 
+    {};
+}
